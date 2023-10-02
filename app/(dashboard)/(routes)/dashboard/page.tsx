2a1d4c16d944ce3284c5f975e0aaaba681eb3924
pantharev <narev001@fiu.edu>
2023-10-01 21:42:47 -0400

    added clerk authentication

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function DashboardPage() {
+  return (
+    <>
+      <div>
+        <p>Dashboard Page (Protected)</p>
+        <UserButton afterSignOutUrl="/"/>
+      </div>
+
+    </>
+  )
+}
